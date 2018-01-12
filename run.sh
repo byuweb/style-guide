10ff29798b2f1cba8ccc2c94f51e1233fa226307
Joseph Moore <yo.yo.yossarian@gmail.com>
2018-01-12 12:22:54 -0700

    horizontal nav layout

--- run.sh
+++ run.sh
@@ -2,7 +2,5 @@
 
 docker build -f Dockerfile-dev . -t byu-style-guide-dev
 
-docker run --rm -it -p 4000:4000 -p 35729:35729 -v $PWD:/usr/src/app byu-style-guide-dev
-
-#docker run -p 80:4000 -p 35729:35729 -v $(pwd):/site bretfisher/jekyll-serve
+docker run --rm -it -p 4000:4000 -p 35729:35729 -v $PWD:/www byu-style-guide-dev
 
